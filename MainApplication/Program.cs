884b9a1d4c468fa503154c98755a911e0bbfc981
dungx <dungx@DESKTOP-NAQPEID>
2016-11-29 01:14:42 +0700

    Main của chương trình Update

--- MainApplication/Program.cs
+++ MainApplication/Program.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MainLibrary;
 
 namespace MainApplication
 {
+    /// <summary>
+    /// Class Program.
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Thực thi chương trình Update.
+        /// </summary>
+        /// <param name="args">Arguments.</param>
         static void Main(string[] args)
         {
+            var objectExecute = new UpdateApplication();
+            objectExecute.Execute();
+
+            Console.ReadKey();
         }
     }
 }
